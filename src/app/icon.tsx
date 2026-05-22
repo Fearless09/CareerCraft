d8e0e54e0b7e9c1980ce2af70ca17c8c2559c8b0
Fearless09 <toheebopeyemi9@gmail.com>
2026-05-22 23:26:17 +0100

    feat: add custom icon generation and initialize change log while removing boilerplate public assets

---
+++ src/app/icon.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    // ImageResponse JSX element
+    <div
+      style={{
+        fontSize: 24,
+        background: "#e94560",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        borderRadius: "8px",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={20}
+        height={20}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-sparkles-icon lucide-sparkles"
+      >
+        <path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594zM20 2v4m2-2h-4" />
+        <circle cx={4} cy={20} r={2} />
+      </svg>
+    </div>,
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  );
+}
